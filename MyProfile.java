dc177206847894bc0f0a40206f9c2ea56065a1ca
azzraa <azrkor26@gmail.com>
2024-06-10 19:08:47 +0200

    codecademy

---
+++ MyProfile.java
@@ -0,0 +1,10 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Azra";
+    int age = 19;
+    double desiredSalary = 1,000,000,000;
+    char gender = 'f';
+    boolean lookingForJob = false;
+
+	}
+}
