9b84f2f66e29a963d2532be76af1add98204c718
azzraa <146182142+azzraa@users.noreply.github.com>
2024-06-10 19:40:59 +0200

    Update BadNames.java

--- BadNames.java
+++ BadNames.java
@@ -1,10 +1,10 @@
 public class BadNames {
 	public static void main(String[] args) {   
 		String firstName = "Samira";
-        String lastName = "Smith";
-        String Mail = "samira@google.com";
-        int salaryexpectation = 100000;
-        int yearBirth = 1955;
+	        String lastName = "Smith";
+	        String Mail = "samira@google.com";
+	        int salaryexpectation = 100000;
+	        int yearBirth = 1955;
         
         System.out.println("The program runs!");
 	}
