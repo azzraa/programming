96acc695b32284daf478cb33b4836db7932a65f6
azzraa <azrkor26@gmail.com>
2024-06-10 19:37:06 +0200

    update

--- MyProfile.java
+++ MyProfile.java
@@ -2,7 +2,7 @@ public class MyProfile {
 	public static void main(String[] args) {   
     String name = "Azra";
     int age = 19;
-    double desiredSalary = 1,000,000,000;
+    double desiredSalary = 1000000000.00;
     char gender = 'f';
     boolean lookingForJob = false;
 
