387e5e398c8980d22679ae022067269fde7a4ad0
azzraa <azrkor26@gmail.com>
2024-06-11 19:58:31 +0200

    codecademy

---
+++ codecademy/Arrays/Newsfeed/Newsfeed.java
@@ -0,0 +1,23 @@
+public class Newsfeed {
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    public int[] views = {0, 0, 0, 0};
+    
+    public Newsfeed(){
+  
+    }
+      
+    public String[] getTopics(){
+      return topics;
+    }
+    
+    // Create getFirstTopic() below
+    public String getFirstTopic() {
+      return topics[0];
+    }
+  
+    
+    public void viewTopic(int topicIndex){
+      views[topicIndex] ++;
+      
+    }
+  }
