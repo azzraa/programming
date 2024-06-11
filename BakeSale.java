7ee6b06243736d71a88b93429e3d94e9446a4e85
azzraa <azrkor26@gmail.com>
2024-06-11 09:41:48 +0200

    codecademy

---
+++ BakeSale.java
@@ -0,0 +1,11 @@
+public class BakeSale {
+	public static void main(String[] args) {   
+		int numCookies = 17;
+        int numCookies -= 3;
+        int numCookies /= 2;
+
+
+    // Add your code above
+    System.out.println(numCookies);
+	}
+}
