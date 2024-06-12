f9e5357ebc8aaf7031ca3e750341e0f55f04ed42
azzraa <azrkor26@gmail.com>
2024-06-12 18:46:53 +0200

    arrays

---
+++ codecademy/Arrays/AnotherNewsfeed/AnotherNewsfeed.java
@@ -0,0 +1,15 @@
+public class AnotherNewsfeed {
+  String[] topics = {"Opinion", "Tech", "Science", "Health"};
+  int[] views = {0, 0, 0, 0};
+  String[] favoriteArticles;
+  
+  public AnotherNewsfeed(){
+    favoriteArticles = new String[10];
+    
+  }
+  
+  public void setFavoriteArticle(int favoriteIndex, String newArticle){
+    favoriteArticles[favoriteIndex] = newArticle;
+    
+  }
+}
