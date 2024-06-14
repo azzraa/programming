b91309bcc3df76080ae02d3dd325e96a319a35ae
azzraa <azrkor26@gmail.com>
2024-06-14 09:16:03 +0200

    string methods

---
+++ codecademy/String Methods/CombineNames.java
@@ -0,0 +1,13 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "";  
+    String lastName = "";
+
+    // What's the full name?
+    System.out.println(firstName.concat(" " + lastName));  
+  }
+  
+}
---
+++ codecademy/String Methods/Password.java
@@ -0,0 +1,9 @@
+public class Password {
+  
+    public static void main(String[] args) {
+      
+      String password = "correcthorsebatterystaple";
+      
+      System.out.println(password.equals("correcthorsebatterystaple"));
+    } 
+  }
