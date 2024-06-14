863362e2e094ff8fdb2aa7367c6fc283895664a2
azzraa <azrkor26@gmail.com>
2024-06-14 10:16:33 +0200

    methods

---
+++ codecademy/Access, Encapsulation and Scope/Scope/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+      SavingsAccount zeusSavingsAccount = new SavingsAccount("Zeus", 1000);
+      
+      zeusSavingsAccount.addMoney(2000);
+    }
+  }
---
+++ codecademy/Access, Encapsulation and Scope/Scope/SavingsAccount.java
@@ -0,0 +1,20 @@
+public class SavingsAccount{
+
+    public String owner;
+    public double balanceDollar;
+    public double balanceEuro;
+  
+    public SavingsAccount(String owner, double balanceDollar){
+      this.owner = owner;
+      this.balanceDollar = balanceDollar;
+      this.balanceEuro = balanceDollar * 0.85;
+  
+    }
+  
+    public void addMoney(int balanceDollar){
+      this.balanceDollar += balanceDollar;
+      System.out.println("Adding " + balanceDollar + " dollars to the account.");
+      System.out.println("The new balance is " + this.balanceDollar + " dollars.");
+    }
+  
+  }
