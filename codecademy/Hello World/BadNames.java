28289cfb1e0f64496f5de3ec26c88678d7ce6800
azzraa <azrkor26@gmail.com>
2024-06-15 22:03:14 +0200

    random

--- codecademy/Hello World/BadNames.java
+++ codecademy/Hello World/BadNames.java
@@ -3,7 +3,7 @@ public class BadNames {
 		String firstName = "Samira";
 	        String lastName = "Smith";
 	        String Mail = "samira@google.com";
-	        int salaryexpectation = 100000;
+	        int salaryexpectation = 1000000;
 	        int yearBirth = 1955;
         
         System.out.println("The program runs!");
