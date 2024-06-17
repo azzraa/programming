6d6d41732ee7113a40832b0542b355ef2f4668fa
azzraa <azrkor26@gmail.com>
2024-06-17 22:44:57 +0200

    2D Arrays

---
+++ codecademy/Two-Dimensional Arrays/TicTacToe.java
@@ -0,0 +1,11 @@
+public class TicTacToe {
+	public static void main(String[] args) {
+		float[][] floatTwoD; 
+    floatTwoD = new float[4][10];
+
+    char[][] ticTacToe = {{'X', 'O', 'O'}, {'O', 'X', ' '}, {'X', ' ', 'X'}};
+    }
+
+   ticTacToe = new char[][] {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', 'O'}};
+    }
+}
