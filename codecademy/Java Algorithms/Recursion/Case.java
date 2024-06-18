787d1937e3114e3ce910388f2fbe7dd96d84c914
azzraa <azrkor26@gmail.com>
2024-06-18 11:35:53 +0200

    recursion

---
+++ codecademy/Java Algorithms/Recursion/Case.java
@@ -0,0 +1,15 @@
+public class Case {
+	public static int recursiveFactorial(int n) {
+		if (n > 0) {
+			System.out.println("Execution context: " + n);
+      return recursiveFactorial(n - 1) * n;
+		}
+
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int recursiveSolution = recursiveFactorial(4);
+		System.out.println(recursiveSolution);
+	}
+}
