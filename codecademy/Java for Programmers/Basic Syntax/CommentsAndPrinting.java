9de43fd2d074f699f1874831542f462d37a772c2
azzraa <azrkor26@gmail.com>
2024-06-21 10:41:57 +0200

    java for programmers

---
+++ codecademy/Java for Programmers/Basic Syntax/CommentsAndPrinting.java
@@ -0,0 +1,15 @@
+public class CommentsAndPrinting {
+    /*
+    This is the main method:
+    */
+    public static void main(String[] args) {
+      // This will end with new line:
+      System.out.println("Hello world!");
+      System.out.println("Howdy y'all!");
+      System.out.println("Hey friends!");
+      // This one will be an introduction
+      System.out.println("Name: Azra");
+      System.out.println("Age: On wednesday I will be 20 ...");
+      System.out.println("That's it. Thanks!");
+    }
+  }
---
+++ codecademy/Java for Programmers/Basic Syntax/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+      System.out.println("Today I will learn the Java Basics just to get a better understanding.");
+      System.out.println("I hope this semester will go well ...");
+    }
+  }
+  
