7ea9d4a9d3b8857fe5dd42d1e7c7d14de2f75300
azzraaa <azrkor26@gmail.com>
2024-07-12 09:50:50 +0200

    Multithreading

---
+++ tutorials/Multithreading/MultithreadThing.java
@@ -0,0 +1,23 @@
+package tutorials.Multithreading;
+
+public class MultithreadThing implements Runnable {
+    
+    private int threadNumber;
+    public MultithreadThing(int threadNumber) {
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 1; i<=5; i++) {
+            System.out.println(i + " from thread " + threadNumber);
+         
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+
+    }
+}
---
+++ tutorials/Multithreading/Multithreading.java
@@ -0,0 +1,12 @@
+package tutorials.Multithreading;
+
+public class Multithreading {
+
+    public static void main(String[] args) {
+        for(int i = 0; i<5; i++) {
+            MultithreadThing myThing = new MultithreadThing(i);
+            Thread myThread = new Thread(myThing);
+            myThread.start();
+        }
+    }   
+}
