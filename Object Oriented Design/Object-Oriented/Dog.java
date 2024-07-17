8870edb9595dd02dcb265c3165f5f41c8becd790
azzraaa <azrkor26@gmail.com>
2024-07-17 22:11:14 +0200

    ordner

---
+++ Object Oriented Design/Object-Oriented/Dog.java
@@ -0,0 +1,21 @@
+public class Dog {
+    private String name;
+  
+    public Dog(String name) {
+      this.name = name;
+    }
+  
+    public String getName() {
+      return name;
+    }
+  
+    public void setName(String newName) {
+      name = newName;
+    }
+  
+    public static void main(String[] args) {
+      Dog myDog = new Dog("Lassie");
+      System.out.println(myDog.getName());
+    }
+  
+  }
