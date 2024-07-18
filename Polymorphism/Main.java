e7ab13bb0968138e095daefa742b702f4051b93d
azzraaa <azrkor26@gmail.com>
2024-07-18 17:08:58 +0200

    polymorphism

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -1,4 +1,25 @@
-package Polymorphism.Animal;
+// Base class
+class Animal {
+    void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+// Derived class
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
+// Derived class
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Meow");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
